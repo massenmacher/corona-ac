da5b479cf959163d46515c2ebd4a6ab6f0022fce
Marian Assenmacher <massenmacher@netcologne.de>
2020-10-08 00:17:11 +0200

    Add some graphs

--- views/dashboard.py
+++ views/dashboard.py
@@ -17,3 +17,85 @@ def dashboard_home():
         columns = []
 
     return render_template("Dashboard/dashboard.html", entries=case_data_entries, columns=columns)
+
+
+@dashboard.route("/active")
+def dashboard_active():
+    query = db_session.query(CaseDataEntry)
+    case_data_entries = query.order_by(asc(CaseDataEntry.timestamp)).all()
+
+    timestmp = list(map(lambda e: e.timestamp, case_data_entries))
+    infected = list(map(lambda e: e.cases_region, case_data_entries))
+    deceased = list(map(lambda e: e.deaths, case_data_entries))
+    recoverd = list(map(lambda e: e.recovered, case_data_entries))
+
+    active_cases = []
+    for i in range(len(case_data_entries)):
+        active = infected[i] - (recoverd[i] or 0) - (deceased[i])
+        active_cases.append((active, timestmp[i]))
+
+
+    return render_template("Dashboard/aspect_graph.html", entries=active_cases, label="Active Cases")
+
+
+@dashboard.route("/newcases")
+def dashboard_newcases():
+    query = db_session.query(CaseDataEntry)
+    case_data_entries = query.order_by(asc(CaseDataEntry.timestamp)).all()
+
+    timestmp = list(map(lambda e: e.timestamp, case_data_entries))
+    infected = list(map(lambda e: e.cases_region, case_data_entries))
+    
+    infected.insert(0,0)
+
+    new_cases = []
+    for i in range(len(case_data_entries)):
+        diff = infected[i+1] - infected[i]
+        new_cases.append((diff, timestmp[i])) 
+
+    return render_template("Dashboard/aspect_graph.html", entries=new_cases, label="New Cases")
+
+@dashboard.route("/newcases/day")
+def dashboard_newcases_meanday():
+    query = db_session.query(CaseDataEntry)
+    case_data_entries = query.order_by(asc(CaseDataEntry.timestamp)).all()
+
+    timestmp = list(map(lambda e: e.timestamp, case_data_entries))
+    infected = list(map(lambda e: e.cases_region, case_data_entries))
+    
+    infected.insert(0,0)
+
+    new_cases = []
+    for i in range(2, len(case_data_entries)):
+        diff = infected[i+1] - infected[i]
+        timedelta = (timestmp[i]-timestmp[i-1])
+        diff /= (timedelta.total_seconds() / (60*60*24))
+        new_cases.append((diff, timestmp[i])) 
+
+    return render_template("Dashboard/aspect_graph.html", entries=new_cases, label="New Cases")
+
+@dashboard.route("/incidence")
+def dashboard_incidence():
+    CITIZENS = 557026 # For Aachen city: 258816
+
+    query = db_session.query(CaseDataEntry)
+    case_data_entries = query.order_by(asc(CaseDataEntry.timestamp)).all()
+
+    timestmp = list(map(lambda e: e.timestamp, case_data_entries))
+    infected = list(map(lambda e: e.cases_region, case_data_entries))
+    
+    infected.insert(0,0)
+
+    new_cases = []
+    for i in range(len(case_data_entries)):
+        diff = infected[i+1] - infected[i]
+        new_cases.append(diff) 
+    
+    incidences = []
+    for i in range(7, len(case_data_entries)):
+        summed = sum(new_cases[i-7:i])/(CITIZENS/100000)
+        incidences.append((summed, timestmp[i]))
+
+    print(incidences)
+
+    return render_template("Dashboard/aspect_graph.html", entries=incidences, label="7 Day Incidence")
